9c8c6216a1924aac0d2ec992bd1918c4809fefd7
hhha <2453174364@qq.com>
2018-03-01 17:07:40 +0800

    商品的上传及以列表的形式显示

---
+++ my-project/model/GoodsModel.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+//创建文档的定义
+var Goods = new Schema({
+    goods_name  : String,
+    goods_sn    : String,
+    goods_price : String,
+    virtual_sales  : String,
+    goods_img01Name  : String,
+    goods_img02Name  : String,
+    create_date :  { type: Date, default: Date.now }
+});
+
+//创建model对象，与数据库中的文档（表）映射
+var GoodsModel = mongoose.model("goods",Goods);
+
+module.exports = GoodsModel;
--- my-project/routes/index.js
+++ my-project/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var md5 = require("md5");
 var UserModel = require("../model/UserModel");
+var GoodsModel = require("../model/GoodsModel");
 var multiparty = require('multiparty');
 
 /* GET home page. */
@@ -18,8 +19,42 @@ router.get('/goods_add', function(req, res, next) {
 });
 
 router.get('/goods_list', function(req, res, next) {
-  res.render('goods_list', {});
+  GoodsModel.find({},function(err,docs){
+  	res.render("goods_list",{list:docs});
+  })
 });
+
+router.post("/api/add_goods",function(req,res){
+	var form = new multiparty.Form({
+		uploadDir : "./public/imgs"
+	})
+	form.parse(req,function(err,body,files){
+		var goods_name = body.goods_name[0];
+		var goods_sn = body.goods_sn[0];
+		var goods_price = body.goods_price[0];
+		var virtual_sales = body.virtual_sales[0];
+		var goods_img01Name = files.goods_img[0].path;
+		var goods_img02Name = files.goods_img[1].path;
+		goods_img01Name = goods_img01Name.substr(goods_img01Name.lastIndexOf("\\")+1);
+		goods_img02Name = goods_img02Name.substr(goods_img02Name.lastIndexOf("\\")+1);
+		console.log(goods_name, goods_sn, goods_price, virtual_sales, goods_img01Name, goods_img02Name);
+        var gm = new GoodsModel();
+        gm.goods_name = goods_name;
+        gm.goods_sn = goods_sn;
+        gm.goods_price = goods_price;
+        gm.virtual_sales = virtual_sales;
+        gm.goods_img01Name = goods_img01Name;
+        gm.goods_img02Name = goods_img02Name;
+        gm.save(function(err){
+        	if(!err){
+        		res.send("添加商品成功");
+        		console.log("添加商品成功");
+        	}else{
+        		res.send("添加商品失败");
+        	}
+        })
+	})
+})
 
 router.post("/api/login",function(req,res){
 	var username = req.body.username;;
